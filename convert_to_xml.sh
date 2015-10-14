8dd1fdbefb00addb828c903607bd01f22e051ce7
Adam Ruul <adam.ruul@gmail.com>
2015-10-14 13:54:31 +0200

    added updated userdatafile etc.

--- convert_to_xml.sh
+++ convert_to_xml.sh
@@ -5,10 +5,10 @@
 
 
 cd $1
-for f in *; do 
+for f in *.msh; do
     dolfin-convert --output xml "$f" "${f%.*}.xml"
 done
-
+echo "*** Moving files ***"
 for x in *.xml; do 
     mv "$x" ../xml
 done
