b88f33c9e12a055429a50dd611d75e02f73cc08e
Adam Ruul <adam.ruul@gmail.com>
2015-10-19 19:54:05 +0200

    minor fixes

--- cw2.py
+++ cw2.py
@@ -128,7 +128,7 @@ def start_broker(bro_ip):
     cmd = "cd /home/ubuntu/ACC_Project/;celery flower -A worker_tasks --address=0.0.0.0 --port=5000"
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    sshkey = paramiko.RSAKey.from_private_key_file(PRIV_KEY_PATH))
+    sshkey = paramiko.RSAKey.from_private_key_file(PRIV_KEY_PATH)
     try:
         ssh.connect(str(bro_ip), username='ubuntu', pkey=sshkey)
         print "*** SSH Connection Established to: "+str(bro_ip)+" ***"
