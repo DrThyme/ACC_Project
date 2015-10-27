6cf4b89cef734b36c9ebbbe93ec888e7c05e3b1c
Adam Ruul <adam.ruul@gmail.com>
2015-10-27 13:52:25 +0100

    now counting lines

--- w_worker_tasks.py
+++ w_worker_tasks.py
@@ -84,6 +84,18 @@ def calc_lift_force(ang):
         p = Popen(['./naca_airfoil/navier_stokes_solver/airfoil',"10","0.0001","10.","1",xmlfile])
         #p = Popen(["./naca_airfoil/navier_stokes_solver/airfoil 10 0.0001 10. 1 "+str(xmlfile)])
         p.wait()
+        line_count = 0
+        m_time = 90
+        t_time = 0
+        while line_count < 220:
+            if t_time > m_time:
+                break
+            else:
+                time.sleep(5)
+                with open('/home/ubuntu/ACC_Project/'+str(angle)+'_results/drag_ligt.m') as f:
+                    line_count = sum(1 for _ in f)
+                t_time += 5
+
     except CalledProcessError as e:
         print e
 
